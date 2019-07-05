f1216c730de395822c6ecb8af96edefacf8996b7
hppRC <hpp.ricecake@gmail.com>
2019-07-05 23:53:10 +0900

    add some solutions

---
+++ EDPC/EDPCF.py
@@ -0,0 +1,59 @@
+#!usr/bin/env python3
+from collections import defaultdict
+from collections import deque
+from heapq import heappush, heappop
+from functools import reduce
+import sys
+import math
+import bisect
+import random
+def I(): return int(sys.stdin.readline())
+def F(): return float(sys.stdin.readline())
+def S(): return input()
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LI_(): return [int(x)-1 for x in sys.stdin.readline().split()]
+def LF(): return list(map(float, sys.stdin.readline().split()))
+def LS():return sys.stdin.readline().split()
+def SR(n): return [list(sys.stdin.readline())[:-1] for i in range(n)]
+def IR(n): return [int(sys.stdin.readline()) for i in range(n)]
+def LIR(n): return [list(map(int, sys.stdin.readline().split())) for i in range(n)]
+def LSR(n): return [list(map(list, sys.stdin.readline().split())) for i in range(n)]
+_gcd = lambda x, y: _gcd(y, x%y) if (x%y) else y
+_lcm = lambda x, y: x*y // _gcd(x, y)
+def gcd(*numbers):
+    return reduce(_gcd, numbers)
+def lcm(*numbers):
+    return reduce(_lcm, numbers)
+sys.setrecursionlimit(1000000)
+INF = float('inf')
+mod = 1000000007
+dire4 = [(1,0), (0,1), (-1,0), (0,-1)]
+dire8 = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+
+s = S()
+t = S()
+
+#dp[s][t]
+dp = [[0]*(len(t)+1) for i in range(len(s)+1)]
+
+for i in range(len(s)):
+    for j in range(len(t)):
+        if (s[i] == t[j]):
+            dp[i+1][j+1] = dp[i][j] + 1
+        else:
+            dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+
+i = len(s)
+j = len(t)
+res = ""
+while (i>0  and j>0):
+    if (dp[i][j] == dp[i-1][j]):
+        i -= 1
+    elif (dp[i][j] == dp[i][j-1]):
+        j -= 1
+    else:
+        i -= 1
+        j -= 1
+        res = s[i] + res
+
+print(res)
