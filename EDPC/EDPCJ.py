36958ae6597003cf2e03b0929e1bfc948079e72a
hppRC <hpp.ricecake@gmail.com>
2019-07-07 06:18:52 +0900

    add some solutions

--- EDPC/EDPCJ.py
+++ EDPC/EDPCJ.py
@@ -1,36 +1,43 @@
-#!usr/bin/env python3
-from collections import defaultdict
-from collections import deque
-from heapq import heappush, heappop
-from functools import reduce
 import sys
-import math
-import bisect
-import random
-from functools import lru_cache
-def I(): return int(sys.stdin.readline())
-def F(): return float(sys.stdin.readline())
-def S(): return input()
-def LI(): return list(map(int, sys.stdin.readline().split()))
-def LI_(): return [int(x)-1 for x in sys.stdin.readline().split()]
-def LF(): return list(map(float, sys.stdin.readline().split()))
-def LS():return sys.stdin.readline().split()
-def SR(n): return [list(sys.stdin.readline())[:-1] for i in range(n)]
-def IR(n): return [int(sys.stdin.readline()) for i in range(n)]
-def LIR(n): return [list(map(int, sys.stdin.readline().split())) for i in range(n)]
-def LSR(n): return [list(map(list, sys.stdin.readline().split())) for i in range(n)]
-_gcd = lambda x, y: _gcd(y, x%y) if (x%y) else y
-_lcm = lambda x, y: x*y // _gcd(x, y)
-def gcd(*numbers):
-    return reduce(_gcd, numbers)
-def lcm(*numbers):
-    return reduce(_lcm, numbers)
-sys.setrecursionlimit(1000000)
-INF = float('inf')
-mod = 1000000007
-dire4 = [(1,0), (0,1), (-1,0), (0,-1)]
-dire8 = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
 
+def I(): return int(sys.stdin.readline())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+
+#実家DPと気合い遷移式
+
+def main():
+    N = I()
+    a = LI()
+    t = [0, 0, 0]
+    for ai in a:
+        t[ai-1] += 1
+    A, B, C = t
+    E = B+C
+
+    dp = [[0 if (not i|j) else N/(i+j) for j in range(E+1)] for i in range(N+1)]
+
+    for k in range(C+1):
+        for j in range(E+1-k):
+            for i in range(N+1-k-j):
+                z = i+j+k
+                if (not z):
+                    continue
+                d = dp[i][j]
+                if (i<N):
+                    dp[i+1][j] += (i+1)*d/(z+1)
+                if (i>0)&(j<E):
+                    dp[i-1][j+1] += (j+1)*d/z
+                if (j>0)&(k<C):
+                    dp[i][j-1] = (k+1)*d/z + N/z
+
+    print(dp[A][B])
+
+if __name__ == "__main__":
+    main()
+
+
+
+"""
 def main():
     N = I()
     A = LI()
@@ -59,7 +66,7 @@ def main():
 
 main()
 
-
+"""
 
 """
 def tail_recursion_with_stack_inspection(g):
