ded2fe190c843759455bdb6726af7f75b84a1978
hppRC <hpp.ricecake@gmail.com>
2019-07-08 04:38:36 +0900

    add some solutions

--- count.sh
+++ count.sh
@@ -7,15 +7,21 @@ AtCoderの問題を解いた際のプログラムです。
 解法の記録と研究・忘却阻止用のリポジトリです。
 
 organize.shは整理整頓用のスクリプトファイルです。easy.shはソースコードを楽にgit pushしたいがためのファイルになっています。
+
 "
+
 today=$(date "+%Y年%m月%d日")
 echo "----------以下現在のファイル数----------
-${today}現在"
+
+${today}現在
+
+"
 
 for f in *
 do
     if [ ${f} != "organize.sh" ] && [ ${f} != "README.md" ] && [ ${f} != "easy.sh" ] && [ ${f} != ".gitignore" ] && [ ${f} != "count.sh" ]
         then echo "${f}"
         ls -1UR $f |wc -l
+        echo "    "
     fi
 done
