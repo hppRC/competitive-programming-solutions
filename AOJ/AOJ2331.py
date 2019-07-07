51c4c1f86984dbce564c3fcdadccd373ed65a4f7
hppRC <hpp.ricecake@gmail.com>
2019-07-08 04:58:20 +0900

    add some solutions

--- AOJ/AOJ2331.py
+++ AOJ/AOJ2331.py
@@ -2,12 +2,18 @@ def main():
     N = int(input())
     ab = [list(map(int, input().split())) for i in range(N)]
 
-    imos = [0]*(N+2)
+    imos = [0]*(N+3)
     for a, b in ab:
-        imos[a] += 1
-        imos[min(b,N+1)] -= 1
+        imos[min(a,N+2)] += 1
+        imos[min(b+1,N+2)] -= 1
     for i in range(N+1):
         imos[i+1] += imos[i]
-    print(imos)
+    #print(imos)
+
+    ans = 0
+    for i, imo in enumerate(imos):
+        if (imo+1 >= i):
+            ans = i-1
+    print(ans)
 
 main()
