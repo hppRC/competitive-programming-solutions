2d3e0bd0f0b888413ae3b630aaecee30eac16f3d
hppRC <hpp.ricecake@gmail.com>
2019-08-14 20:30:41 +0900

    add some solutions

---
+++ ABC/ABC-C/ABC048C.rs
@@ -0,0 +1,86 @@
+#![allow(non_snake_case)]
+#[allow(unused_imports)]
+use std::io::{self, Write};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::cmp::{max, min, Ordering};
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+     };
+     ($next:expr, $t:ty) => {
+         $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(dead_code)]
+const MOD: u64 = 1000000007;
+#[allow(dead_code)]
+fn to_num(c: char) -> i64 {
+    c as i64 - 48
+}
+
+
+fn main() {
+    input!{
+        N: usize, x: usize,
+        A: [usize; N]
+    }
+    let mut ans = 0;
+    let mut B = vec![0; N];
+
+    B[0] = if A[0] > x {
+        ans += A[0] - x;
+        x
+    } else {
+        A[0]
+    };
+
+    for i in 1..N {
+        B[i] = if B[i-1] + A[i] > x {
+            ans += B[i-1] + A[i] - x;
+            x - B[i-1]
+        } else {
+            A[i]
+        };
+    }
+    println!("{:?}", ans);
+}
