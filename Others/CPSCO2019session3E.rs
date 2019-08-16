d40c21f393e2aaec3d3c2cd6e08bc0b3e446a20a
hppRC <hpp.ricecake@gmail.com>
2019-08-16 14:25:21 +0900

    add some solutions

--- Others/CPSCO2019session3E.rs
+++ Others/CPSCO2019session3E.rs
@@ -74,16 +74,37 @@ fn main() {
         N: usize,
         A: [usize; N],
     }
-    let mut acc = vec![0; N];
-    acc[0] = A[0];
-    for i in 1..N {
-        acc[i] = A[i] ^ acc[i-1];
-    }
-    let mut back = vec![0; N];
-    for i in 0..N {
-        back[i] = acc[N-1] ^ A[i];
-    }
-    let mut accxor = vec![0; N];
+    //最大桁数(A < 2**30なので)
+    const DIGIT: usize = 30;
+    //j桁目の1の個数を格納
+    let mut cnt = vec![0; DIGIT];
 
-    debug!(acc);
+    //累積xor
+    let mut X = 0;
+
+    for i in 0..N {
+        //各iごとにAと今までの累積xorをとる
+        X ^= A[i];
+        let mut res = 0;
+        for j in 0..DIGIT {
+            //A[i]のj桁目が1ならcnt[j]を1増やす
+            if (A[i] >> j) & 1 == 1 {
+                cnt[j] += 1;
+            }
+            //累積xorのj桁目が1なら
+            if (X >> j) & 1 == 1 {
+                //各A[0..i]のj桁目は1とのxorをとることになる
+                //なので,今までのj桁目に存在する0の個数だけ,A[0..i]においてj桁目に1が立つ
+                //j桁目だけを考えてあげれば, A[0..i] xor X の結果はj桁目に0が立っているものの総数 * 2をj-1乗(1 << j)したものになる
+                //また,cnt[j]には今までのj桁目の1の個数が入っているので, 0の個数は i - cnt[j] で求まる. よって以下のようになる
+                res += (i + 1 - cnt[j]) * (1 << j);
+            } else {
+                //累積xorのj桁目が0なら
+                //各A[0..i]のj桁目は0とのxorをとることになる, つまりそのまま
+                //上記同じように足してあげる
+                res += cnt[j] * (1 << j);
+            }
+        }
+        println!("{}", res);
+    }
 }
